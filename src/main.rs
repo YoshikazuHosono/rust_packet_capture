11e939dd60873a1c7eb91113004377b6033ab731
YoshikazuHosono <yyhhwork@gmail.com>
2022-01-11 22:29:34 +0900

    save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate pnet;
+
+use pnet::datalink::{self, NetworkInterface};
+use pnet::datalink::Channel::Ethernet;
+
 fn main() {
-    println!("Hello, world!");
+
+    let iface_name = String::from("ap1");
+    let interface_names_match = |iface: &NetworkInterface| iface.name == iface_name;
+
+    // Find the network interface with the provided name
+    let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .filter(interface_names_match)
+        .next()
+        .unwrap_or_else(|| panic!("No such network interface: {}", iface_name));
+
+    // Create a channel to receive on
+    let (_, _rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("packetdump: unhandled channel type"),
+        Err(e) => panic!("packetdump: unable to create channel: {}", e),
+    };
+
+    println!("main method end");
 }
